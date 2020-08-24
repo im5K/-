13cdcff4a604da4cf277a660b4ce569c54550e3b
zth.laptop <thzhao@mail.ustc.edu.cn>
2020-08-24 12:25:19 +0800

    p30

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,12 +1,26 @@
 import React ,{Component} from "react"
-import { Redirect } from "react-router-dom"
+import { Redirect,Route,Switch} from "react-router-dom"
 import {Layout} from 'antd'
 
 import memoryUtils from "../../utils/memoryUtils"
 import LeftNav from "../../components/left-nav"
- import Header from "../../components/header"
+import Header from "../../components/header"
+
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import Role from "../role/role"
+import User from '../user/user'
+import Pie from '../chart/pie'
+import Bar from '../chart/bar'
+import Line from '../chart/line'
+
+
+
+
 
 const{Footer,Sider,Content}=Layout
+
 
 export default class Admin extends React.Component{
 
@@ -24,7 +38,21 @@ export default class Admin extends React.Component{
             </Sider>
             <Layout>
             <Header>Header</Header>
-            <Content style={{backgroundColor: "white"}}>Content</Content>
+            <Content style={{backgroundColor: "white"}}>
+                <Switch>
+                    <Route path='/home' component={Home}/>
+                    <Route path='/category' component={Category}/>
+                    <Route path='/product' component={Product}/>
+                    <Route path='/role' component={Role}/>
+                    <Route path='/user' component={User}/>
+                    <Route path='/charts/bar' component={Bar}/>
+                    <Route path='/charts/line' component={Line}/>
+                    <Route path='/charts/pie' component={Pie}/>
+                    {/* {不是请求首页就返回home用redirect} */}
+                    <Redirect to='/home' />
+                </Switch>
+                
+            </Content>
             <Footer style={{textAlign: "center",color: "#cccccc"}} >推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
             </Layout>
             </Layout>
