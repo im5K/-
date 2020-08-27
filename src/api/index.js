521fac9b1a3a39d9fd3e7c0518ad7030932d7cb9
zth.laptop <thzhao@mail.ustc.edu.cn>
2020-08-27 22:34:35 +0800

    P48

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,18 @@ import ajax from "./ajax"
 import jsonp from "jsonp";
 import { message } from "antd";
 
+const BASE = ""
 export const reqLogin = (username,password)=>
 ajax("/login",{username,password},'POST')
 //添加用户
 export const ReqAddUser = (user)=>
 ajax('/manage/user/add',user,'POST')
+//获取1级/二级分类的列表
+export const reqCategorys=(parentId)=> ajax(BASE+'manage/category/list',{parentId:parentId})
+//添加分类
+export const reqaddCategory=(categoryName,parentId)=> ajax(BASE+'manage/category/add',{categoryName,parentId},'POST')
+//添加分类
+export const reqUpdateCategory=({categoryId,categoryName})=> ajax(BASE+'manage/category/update',{categoryId,categoryName},'POST')
 
 /*
 json请求接口请求函数
@@ -34,4 +41,5 @@ export const reqWeather = (city)=>{
    
 
 }
+
 //reqWeather("北京")
--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,18 +1,96 @@
 import React, {Component} from 'react'
-import {reqWeather} from "../../api/index"
+import { Card, Icon, Button, Table, message } from 'antd'
+
+import LinkButton from '../../components/link-button'
+import {reqCategorys} from "../../api"
 export default class Category extends Component{
+    state = {
+        loading:false,
+        categorys:[ ],//一级分类列表
+
+
+    }
+ //初始化table所有列的数组
+ initColumns=()=>{
+    this.columns = [
+        {
+          title: '分类名称',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: '操作',
+          width:300,
+          render: ()=>(
+              <span>
+                  <LinkButton>修改分类</LinkButton>
+                  <LinkButton>查看子分类</LinkButton>
+
+              </span>
+          )
+        },
+     
+      ];
+
+ }
+ //为第一次render准备数据
+    componentWillMount(){
+        this.initColumns();
+       
+    }
+    //执行异步任务
+    componentDidMount(){
+        this.getCategorys();
+       
+    }
+    getCategorys = async () =>{
+        //发请求前loading
+        this.setState({loading:true})
+        //发异步ajax请求，获取数据
+        const result =await reqCategorys('0')
+        //完成后，隐藏loading
+        this.setState({loading:false})
+        if(result.status===0){
+                const categorys = result.data
+                //更新状态
+               
+
+                this.setState({categorys})
+        } else {
+            message.error('获取分类列表失败')
+        }
+
+    }
+
+
     render(){
-       
-        const weather =  reqWeather("合肥")
-           
-        console.log(weather)
-       
+        //card 左侧
+       const title ="一级分类列表"
+        //card 右侧
+       const extra = (
+          
+           <Button type='primary'>
+               <Icon type="plus"></Icon>
+               添加
+           </Button>
+       )
+
+       const {categorys,loading} = this.state
+      
+     
+      
         return(
-            <span>
-             
+            
+            <Card title={title} extra={extra} >
+                <Table 
+                bordered 
+                rowKey='_id' 
+                loading={loading}
+                dataSource={categorys} 
+                columns={this.columns} 
+                pagination={{defaultPageSize:5,showQuickJumper:true}}/>
 
-
-            </span>
+            </Card>
         )
     }
 }
