1e8d0de9a3538d383d8b98d4f9295255cb5f3267
zth.laptop <thzhao@mail.ustc.edu.cn>
2020-08-28 17:50:23 +0800

    P58

---
+++ src/pages/product/home.jsx
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react'
+/*
+默认子路由界面
+*/
+export default class ProductHome extends Component{
+    render(){
+        return(
+            <div>
+                ProductHome
+            </div>
+        )
+    }
+}
--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -1,12 +1,19 @@
 
 import React, {Component} from 'react'
+import {Switch,Route, Redirect} from 'react-router-dom'
 
+import ProductHome from './home'
+import ProductAddUpdate from './add-update'
+import ProductDetail from './detail'
 export default class Product extends Component{
     render(){
         return(
-            <div>
-                Product
-            </div>
+            <Switch>
+                <Route exact path='/product' component={ProductHome}/>
+                <Route  path='/product/addupdate' component={ProductAddUpdate}/>
+                <Route path='/product/detail' component={ProductDetail}/>
+                <Redirect path="/product"/>
+            </Switch>
         )
     }
 }
